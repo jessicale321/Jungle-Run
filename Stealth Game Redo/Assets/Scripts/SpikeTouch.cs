a9f5b9ab249a59f6b58f360dbf3af352735d0de3
Jessica <jessicale321@gmail.com>
2022-07-09 17:02:02 -0700

    Rescale UI and Fix spikes
    UI has TMP and scales better across different screen sizes
    Level will reload when player touches spikes using GameManager script

--- Stealth Game Redo/Assets/Scripts/SpikeTouch.cs
+++ Stealth Game Redo/Assets/Scripts/SpikeTouch.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class SpikeTouch : MonoBehaviour
+namespace DefaultNamespace
 {
-    private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("Player")) {
-            Debug.Log("Player touch spike");
-            SceneMngr.reloadLevel();
+    public class SpikeTouch : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other) {
+            if (other.gameObject.CompareTag("Player")) {
+                Debug.Log("Player touch spike");
+                StartCoroutine(GameManager.ReloadLevel());
+            }
         }
     }
 }
